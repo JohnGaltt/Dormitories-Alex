f52349b74181a0ca0ea775bfe93c50c8b1f0dcb2
Oleksandr Zholob <johngaltaregoing@gmail.com>
2020-03-29 22:51:29 +0300

    Stucture Initial Script + Initiad Data

---
+++ db/Initial Script.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE Dormitory;
+
+CREATE TABLE Dormitories(
+Id int IDENTITY(1,1) PRIMARY KEY ,
+Name varchar(64),
+Address varchar(64)
+);
+
+CREATE TABLE Rooms(
+Id int IDENTITY(1,1),
+DormitoryId int, 
+PRIMARY KEY(Id, DormitoryId),
+Name varchar(64),
+Floor int
+);
+
+CREATE TABLE Students(
+Id int IDENTITY(1,1)  PRIMARY KEY ,
+Name varchar(64),
+Email varchar(64),
+RoomId int,
+DormitoryId int,
+FOREIGN KEY (RoomId, DormitoryId) REFERENCES Rooms(Id, DormitoryId),
+FOREIGN KEY (DormitoryId) REFERENCES Dormitories(Id)
+);
+INSERT INTO Dormitories VALUES(
+'Гуртожиток 1', 'Лукаша 1')	
+INSERT INTO Dormitories VALUES(
+'Гуртожиток 2', 'Лукаша 2')
+INSERT INTO Dormitories VALUES(
+'Гуртожиток 3', 'Лукаша 3')
+
+INSERT INTO Rooms VALUES
+(1, '15', 1),
+(2, '16', 2)
+
+INSERT INTO Students VALUES('Василь', 'vasyl@gmail.com', 3, 1)
+INSERT INTO Students VALUES('Петро', 'petro@gmail.com', 3, 1)
